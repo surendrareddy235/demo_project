162187a141f5d0b692e549561c6329fb11ddcfa8
Manjithnadhgit config --global user.email manjithmullapudi@GMAIL.comgit config --global user.name Manjithnadh <manjithmullapudi@gmail.com>
2025-05-19 17:53:23 +0530

    app

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+DATABASE = 'demo.db'
