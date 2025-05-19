29185383382cbbd72d238c5ffac2423be04d4f03
Your Name <your.email@example.com>
2025-05-19 03:55:39 -0700

    included the functions

--- db.py/db.py
+++ db.py/db.py
@@ -1,17 +1,23 @@
 import sqlite3
 
-con = sqlite3.connect("demo.db")
+def init_db():
+    con = sqlite3.connect("demo.db")
 
-cursor = con.cursor()
+    cursor = con.cursor()
 
-cursor.execute('''
-    create table users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT NOT NULL,
-        password TEXT NOT NULL
-    )
-''')
-con.commit()
-con.close()
+    cursor.execute('''
+        create table if not exists users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    con.commit()
+    con.close()
+
+def get_connection():
+    conn = sqlite3.connect('demo.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
