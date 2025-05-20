298699723223c32b3c7539c311d83b5ca9141995
Your Name <your.email@example.com>
2025-05-20 01:48:09 -0700

    correct app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask
-from __init__.db import get_connection
+from __init__.db import get_connection, init_db
+from routes.signuproute import demo
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+app.register_blueprint(demo)
 
 
 with app.app_context():
-    get_conn()
-
-@app.route('/')
-def index():
-    return 'Welcome to the Flask Auth App!'
+    init_db()
 
 if __name__ == '__main__':
     app.run(debug=True)
